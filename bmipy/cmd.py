1861edbe152bae1dd53a6307d1c3ba8587b4b923
mcflugen <mcflugen@gmail.com>
2019-04-30 16:28:01 -0600

    feat: add a --no-hints option to remove type hints

--- bmipy/cmd.py
+++ bmipy/cmd.py
@@ -7,7 +7,9 @@ import jinja2
 from bmipy import Bmi
 
 BMI_TEMPLATE = """# -*- coding: utf-8 -*-
+{% if with_hints -%}
 from typing import Tuple
+{%- endif %}
 
 from bmipy import Bmi
 import numpy
@@ -22,11 +24,22 @@ class {{ name }}(Bmi):
 """
 
 
+def remove_hints_from_signature(signature):
+    """Remove hint annotation from a signature."""
+    params = []
+    for name, param in signature.parameters.items():
+        params.append(param.replace(annotation=inspect.Parameter.empty))
+    return signature.replace(
+        parameters=params, return_annotation=inspect.Signature.empty
+    )
+
+
 @click.command()
 @click.version_option()
-@click.option("--black / --no-black", is_flag=True, help="format output with black")
+@click.option("--black / --no-black", default=True, help="format output with black")
+@click.option("--hints / --no-hints", default=True, help="include type hint annotation")
 @click.argument("name")
-def render_bmi(name, black):
+def render_bmi(name, black, hints):
     """Render a template BMI implementation in Python for class NAME."""
 
     env = jinja2.Environment()
@@ -34,9 +47,12 @@ def render_bmi(name, black):
 
     funcs = {}
     for func_name, func in inspect.getmembers(Bmi, inspect.isfunction):
-        funcs[func_name] = {"sig": inspect.signature(func), "doc": func.__doc__}
+        signature = inspect.signature(func)
+        if not hints:
+            signature = remove_hints_from_signature(signature)
+        funcs[func_name] = {"sig": signature, "doc": func.__doc__}
 
-    contents = template.render(name=name, funcs=funcs)
+    contents = template.render(name=name, funcs=funcs, with_hints=hints)
 
     if black:
         contents = blk.format_file_contents(contents, fast=True, mode=blk.FileMode())
