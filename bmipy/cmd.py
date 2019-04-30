7637605c4229b00a89fc113f68e16f04be6b2bdf
mcflugen <mcflugen@gmail.com>
2019-04-30 17:35:10 -0600

    feat: check to see if the class name is valid

--- bmipy/cmd.py
+++ bmipy/cmd.py
@@ -1,4 +1,6 @@
 import inspect
+import keyword
+import re
 
 import black as blk
 import click
@@ -24,7 +26,7 @@ class {{ name }}(Bmi):
 """
 
 
-def remove_hints_from_signature(signature):
+def _remove_hints_from_signature(signature):
     """Remove hint annotation from a signature."""
     params = []
     for name, param in signature.parameters.items():
@@ -34,27 +36,64 @@ def remove_hints_from_signature(signature):
     )
 
 
+def _is_valid_class_name(name):
+    p = re.compile(r"^[^\d\W]\w*\Z", re.UNICODE)
+    return p.match(name) and not keyword.iskeyword(name)
+
+
+def render_bmi(name, black=True, hints=True):
+    """Render a template BMI implementation in Python
+
+    Parameters
+    ----------
+    name : str
+        Name of the new BMI class to implement.
+    black : bool, optional
+        If True, reformat the source using black styling.
+    hints : bool, optiona
+        If True, include type hint annotation.
+
+    Returns
+    -------
+    str
+        The contents of a new Python module that contains a template for
+        a BMI implementation.
+    """
+    if _is_valid_class_name(name):
+        env = jinja2.Environment()
+        template = env.from_string(BMI_TEMPLATE)
+
+        funcs = {}
+        for func_name, func in inspect.getmembers(Bmi, inspect.isfunction):
+            signature = inspect.signature(func)
+            if not hints:
+                signature = _remove_hints_from_signature(signature)
+            funcs[func_name] = {"sig": signature, "doc": func.__doc__}
+
+        contents = template.render(name=name, funcs=funcs, with_hints=hints)
+
+        if black:
+            contents = blk.format_file_contents(
+                contents, fast=True, mode=blk.FileMode()
+            )
+
+        return contents
+    else:
+        raise ValueError("invalid class name ({0})".format(name))
+
+
 @click.command()
 @click.version_option()
 @click.option("--black / --no-black", default=True, help="format output with black")
 @click.option("--hints / --no-hints", default=True, help="include type hint annotation")
 @click.argument("name")
-def render_bmi(name, black, hints):
+@click.pass_context
+def main(ctx, name, black, hints):
     """Render a template BMI implementation in Python for class NAME."""
-
-    env = jinja2.Environment()
-    template = env.from_string(BMI_TEMPLATE)
-
-    funcs = {}
-    for func_name, func in inspect.getmembers(Bmi, inspect.isfunction):
-        signature = inspect.signature(func)
-        if not hints:
-            signature = remove_hints_from_signature(signature)
-        funcs[func_name] = {"sig": signature, "doc": func.__doc__}
-
-    contents = template.render(name=name, funcs=funcs, with_hints=hints)
-
-    if black:
-        contents = blk.format_file_contents(contents, fast=True, mode=blk.FileMode())
-
-    print(contents)
+    if _is_valid_class_name(name):
+        print(render_bmi(name, black=black, hints=hints))
+    else:
+        click.secho(
+            "💥 💔 💥 '{0}' is not a valid class name in Python".format(name), err=True, fg="red"
+        )
+        ctx.exit(code=1)
