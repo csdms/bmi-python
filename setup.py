edd5f25ec3e2d85ed656f4f8eb8bcfe8526f2cb6
mcflugen <mcflugen@gmail.com>
2019-05-08 10:29:55 -0600

    refactor: adjust setup params for py2/py3

--- setup.py
+++ setup.py
@@ -1,6 +1,29 @@
 from setuptools import find_packages, setup
+import six
 
 import versioneer
+
+
+classifiers=[
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Scientific/Engineering :: Physics",
+]
+install_requires=["black", "click", "jinja2", "numpy"]
+
+if six.PY2:
+    classifiers.extend(
+        [
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+        ]
+    )
+    install_requires.remove("black")
 
 
 setup(
@@ -11,18 +34,9 @@ setup(
     author="Eric Hutton",
     author_email="huttone@colorado.edu",
     url="http://csdms.colorado.edu",
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Scientific/Engineering :: Physics",
-    ],
+    classifiers=classifiers,
     setup_requires=["setuptools"],
-    install_requires=["black", "click", "jinja2", "numpy"],
+    install_requires=install_requires,
     packages=find_packages(),
     cmdclass=versioneer.get_cmdclass(),
     entry_points={"console_scripts": ["bmipy-render=bmipy.cmd:main"]},
