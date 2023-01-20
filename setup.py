515df99c9e3b66fc0c2a001e5c32da5daf300027
Mike Taves <mwtoews@gmail.com>
2023-01-20 15:04:17 +1300

    Drop python 3.6, test 3.10 and 3.11

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(__file__))
 import versioneer
 
 setup(
+    name="bmipy",  # need by GitHub dependency graph
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
 )
