50c43a6dd6acd8e39aa23c6dddd248a1a19f17c9
Mike Taves <mwtoews@gmail.com>
2023-01-24 15:24:26 +1300

    Move static project metadata to pyproject.toml

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(__file__))
 
 import versioneer
 
+# See pyproject.toml for project metadata
 setup(
     name="bmipy",  # need by GitHub dependency graph
     version=versioneer.get_version(),
