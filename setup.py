5b485d2eae9a623e06525d9213caf6940bff5f0b
Mark Piper <mark.piper@colorado.edu>
2023-01-24 11:42:44 -0700

    Scrub versioneer from project metadata files

--- setup.py
+++ setup.py
@@ -4,11 +4,7 @@ from setuptools import setup
 
 sys.path.append(os.path.dirname(__file__))
 
-import versioneer
-
 # See pyproject.toml for project metadata
 setup(
     name="bmipy",  # need by GitHub dependency graph
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
 )
