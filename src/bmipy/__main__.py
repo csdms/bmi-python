284721c09999056df1d241dc28386b5eb9c17088
mcflugen <mcflugen@gmail.com>
2024-01-05 14:44:43 -0700

    remove a bit of lint

--- src/bmipy/__main__.py
+++ src/bmipy/__main__.py
@@ -1,6 +1,7 @@
+"""The bmipy-render command."""
 from __future__ import annotations
 
 from bmipy.cmd import main
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise SystemExit(main())
