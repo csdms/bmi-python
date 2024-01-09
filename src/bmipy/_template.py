ade3dad373aab3b9b6c97852681cbd7d7d5b22d4
mcflugen <mcflugen@gmail.com>
2024-01-08 21:03:20 -0700

    import some type annotations to the generated bmi

--- src/bmipy/_template.py
+++ src/bmipy/_template.py
@@ -19,7 +19,10 @@ class Template:
         prefix = f"""\
 from __future__ import annotations
 
+from typing import Any
+
 import numpy as np
+from numpy.typing import NDArray
 
 from bmipy.bmi import Bmi
 
