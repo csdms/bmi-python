04336e1ba7cccd32951f447ce584b8a5bf628852
mcflugen <mcflugen@gmail.com>
2026-03-28 15:45:39 -0600

    manage package version through pyproject metadata
    * set version in pyproject.toml
    * use importlib to get version

--- src/bmipy/_version.py
+++ src/bmipy/_version.py
@@ -1,3 +1,9 @@
 from __future__ import annotations
 
-__version__ = "2.0.2.dev0"
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version
+
+try:
+    __version__ = version("bmipy")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
